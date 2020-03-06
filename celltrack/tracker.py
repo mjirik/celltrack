857b590791b3716576d8737444c3822482ce3ac0
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-03-06 10:47:04 +0100

    module for tracker

---
+++ celltrack/tracker.py
@@ -0,0 +1,81 @@
+from pyqtgraph.parametertree import Parameter
+# from . import something
+from exsu import Report
+import numpy as np
+import os
+from pathlib import Path
+
+
+path_to_script = Path(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Tracking:
+    def __init__(
+            self,
+            report: Report = None,
+            pname = "Tracking",
+            ptype = "group",
+            pvalue = None,
+            ptip = "Tracking parameters",
+
+    ):
+
+        # TODO Sem prosím všechny parametry.
+        params = [
+
+            {
+                "name": "Example Integer Param",
+                "type": "int",
+                "value": 224,
+                "suffix": "px",
+                "siPrefix": False,
+                "tip": "Value defines size of something",
+            },
+            {
+                "name": "Example Float Param",
+                "type": "float",
+                "value": 0.00006,
+                "suffix": "m",
+                "siPrefix": True,
+                "tip": "Value defines size of something",
+            },
+        ]
+        self.parameters = Parameter.create(
+            name=pname,
+            type=ptype,
+            value=pvalue,
+            tip=ptip,
+            children=params,
+            expanded=False,
+        )
+        if report is None:
+            report = Report()
+            report.save = False
+            report.show = False
+        self.report: Report = report
+        pass
+
+
+    def init(self):
+        model_path = path_to_script / 'models/my_best_model.model' #cesta k ulozenym modelum
+        pass
+
+    def process_image(self, image:np.ndarray, resolution:np.ndarray, time_axis:int=None, z_axis:int=None, color_axis:int=None):
+        # TODO implementation
+
+
+        # examples
+        # get some parameter value
+        sample_weight = float(self.parameters.param("Example Float Param").value())
+
+        # sample output
+        out = {
+            "obj_id": [1, 1, 2],
+            "x_px": [ 100, 100, 100],
+            "y_px": [100, 100, 105],
+            "t_s": [1.0, 2.0, 2.0],
+            "parent_id": [None, None, 1],
+        }
+        return out
+        # return (image > 0.5).astype(np.uint8)
+
